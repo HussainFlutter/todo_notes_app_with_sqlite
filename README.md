10ff7bfaacf92748a5535099865ad2ad24096913
Hussain <157299514+HussainFlutter@users.noreply.github.com>
2024-02-24 22:43:11 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo_notes_app_with_sqlite
+ 
---
+++ android/app/src/main/kotlin/com/example/todo_notes_with_sqlite/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todo_notes_with_sqlite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
