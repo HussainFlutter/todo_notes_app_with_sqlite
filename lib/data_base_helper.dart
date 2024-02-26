9c66b3fdfa009fa5a10dc9bdb3e34f0a81f8294f
Hussain <157299514+HussainFlutter@users.noreply.github.com>
2024-02-26 20:33:43 +0500

    some changes

--- lib/data_base_helper.dart
+++ lib/data_base_helper.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:sqflite/sqflite.dart';
-
 import 'models/note_model.dart';
 
 Database? db;
@@ -8,44 +8,45 @@ class DataBaseHelper {
   static const String tableName = "note";
 
   static Future<void> addNote (NoteModel noteModel) async {
-     await db!.insert(tableName, noteModel.toMap());
-   // final response = await db!.query(tableName,orderBy: "priority ASC");
-    //print(response);
-   // print("here");
+    try{
+      await db!.insert(tableName, noteModel.toMap());
+    }catch(e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
   }
   static Future<void> updateNote (NoteModel noteModel) async {
-   await  db!.update(
-       tableName,
-     noteModel.toMap(),
-     where: "id = ?",
-     whereArgs: [noteModel.id],
-   );
+    try{
+      await  db!.update(
+        tableName,
+        noteModel.toMap(),
+        where: "id = ?",
+        whereArgs: [noteModel.id],
+      );
+    }catch(e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+
   }
   static Future<void> deleteNote (NoteModel noteModel) async {
-    await  db!.delete(
-      tableName,
-      where: "id = ?",
-      whereArgs: [noteModel.id],
-    );
+    try{
+      await  db!.delete(
+        tableName,
+        where: "id = ?",
+        whereArgs: [noteModel.id],
+      );
+    }catch(e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
   }
    Future<List<NoteModel>> getNotes () async {
     try{
       final List<Map<String, Object?>> response = await db!.query(tableName,orderBy: "priority ASC");
-      print(response[0]);
-      print("here123");
-      //var list = NoteModel.fromMap(response[0] as Map<String,dynamic>);
-      print("list");
-      //print(list.toString());
-      List<NoteModel> notes = [];
-      for(int i = 0 ; i<response.length;i++)
-      {
-        notes.add(NoteModel.fromMap(response[i] as Map<String,dynamic>));
-      }
-      print(notes.toString());
-      return notes;
-      //return response.map((e) => NoteModel.fromMap(e as Map<String,Object>)).toList();
+      return response.map((e) => NoteModel.fromMap(e as Map<String,Object>)).toList();
     }catch(e){
-      print(e.toString());
+      debugPrint(e.toString());
       rethrow;
     }
   }
